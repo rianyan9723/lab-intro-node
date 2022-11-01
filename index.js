0f7409293afca49fc62056f406b606995ae13516
Rian Yan <rianyan9723@gmail.com>
2022-11-01 21:50:09 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,80 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length; 
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
 
-  min() {}
+    this.items.pop(item); 
+    this.items = this.items.sort((function(a,b){
+      return a-b;
+    }))
 
-  sum() {}
+  }
 
-  avg() {}
-}
+  get(pos) {
+
+    if (i < this.items.length){
+      return this.items[pos];
+    }
+    else {
+      throw new Error('OutOfBounds');
+    }
+
+  }
+
+  max() {
+
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    
+    else {
+      return this.Math.max(items);
+    }
+
+  }
+
+  min() {
+
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    
+    else {
+      return this.Math.min(items);
+    }
+
+
+  }
+
+  sum() {
+
+    if (this.length === 0) {
+      return 0;
+    }
+
+    return this.items.reduce((acc, inc) => {
+      acc = acc + inc;
+
+      return acc;
+      
+    }, 0);
+    
+  }
+
+  avg() {
+
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.length;
+
+  }
+
 
 module.exports = SortedList;
